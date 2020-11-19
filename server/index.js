cd340a723c25933fe02c91f3ed1cf73fd7f63c33
WRHR <wrhreeves@gmail.com>
2020-11-18 21:57:25 -0700

    Add routes for adding todos, listing all todos

--- server/index.js
+++ server/index.js
@@ -1,10 +1,40 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const pool = require('./db')
 // middleware
 app.use(cors())
 app.use(express.json())
+
+// routes
+
+//create tdo
+app.post('/todos', async (req, res) => {
+  try {
+    const {description} = req.body
+    const newTodo = await pool
+      .query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description])
+
+    res.json(newTodo.rows[0])
+  } catch (err) {
+    console.log(err.message)
+  }
+})
+
+//get all todos
+app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo')
+    res.json(allTodos.rows)
+  } catch (err) {
+    console.log(err.message)
+  }
+})
+// get single todo
+
+//update todo
+
+//delete todo
 
 app.listen(5000, () => {
   console.log('server running on 5000')
