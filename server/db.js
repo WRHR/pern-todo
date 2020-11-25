bdc692036035d3ac1818f3c8bc80a1c1f1ac0883
WRHR <wrhreeves@gmail.com>
2020-11-24 21:06:56 -0700

    Enable prettier

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: 'w',
-  host: 'localhost',
+  user: "w",
+  host: "localhost",
   port: 5432,
-  database: 'perntodo'
-})
+  database: "perntodo",
+});
 
-module.exports = pool
+module.exports = pool;
